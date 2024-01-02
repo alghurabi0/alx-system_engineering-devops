e0fdb4409f598e2686a40050a180954cbf397def
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2024-01-02 17:53:44 +0200

    wtf

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,19 +5,28 @@ import requests
 from sys import argv
 
 
+base_url = "https://jsonplaceholder.typicode.com/"
+
+
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
     user_id = argv[1]
-    user = requests.get(url + 'users/{}'.format(user_id))
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    user = requests.get(user_url)
     userData = user.text
     userData = json.loads(userData)
-    userName = userData.get('name')
-    todos = requests.get(url + 'todos', params={'userId': user_id})
-    tasks = todos.text
-    tasks = json.loads(tasks)
-    completed = [task for task in tasks if task.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):".format(userName,
-                                                          len(completed),
-                                                          len(tasks)))
-    for task in completed:
+    user_name = userData[0].get('name')
+
+    todo_url = '{}/todos?userId={}'.format(base_url, user_id)
+    todo = requests.get(todo_url)
+    todoData = todo.text
+    todoData = json.loads(todoData)
+    completed = 0
+    completed_tasks = []
+    total_tasks = len(todoData)
+    for task in todoData:
+        if task.get('completed'):
+            completed += 1
+            completed_tasks.append(task)
+    print("Employee {} is done with tasks({}/{}):".format(user_name, completed, total_tasks))
+    for task in completed_tasks:
         print("\t {}".format(task.get('title')))

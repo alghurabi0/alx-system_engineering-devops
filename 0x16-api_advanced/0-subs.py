9c4a849f870357354b7bd6f5ceaf478b41f05f9f
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2024-01-16 12:48:10 +0000

    whaaat

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Get number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
-               Safari/537.36"}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
+    r = requests.get(
+            url,
+            headers={"User-Agent": "My-User-Agent"},
+            allow_redirects=False
+            )
+    if r.status_code < 300:
         return r.json().get("data").get("subscribers")
     else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,12 @@ def top_ten(subreddit):
                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77\
                Safari/537.36"}
     params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+            )
     if response.status_code == 200:
         for post in response.json().get('data').get('children'):
             print(post.get('data').get('title'))

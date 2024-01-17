ed9934314b496c0191bc93453f88714a8a217bb5
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2024-01-17 23:25:53 +0000

    list

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixing wordpress site
 
-exec{'fix-wordpress':
+exec { 'fix-apache':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path	  => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/'
 }
